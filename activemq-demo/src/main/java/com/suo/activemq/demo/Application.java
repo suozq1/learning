f59255ff38e3525fe1e92046a3828d29f61397d0
91585 <91585@SUO-RESCUER>
2017-10-16 23:29:45 +0800

    tijiao

--- activemq-demo/src/main/java/com/suo/activemq/demo/Application.java
+++ activemq-demo/src/main/java/com/suo/activemq/demo/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
 public class Application {
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
