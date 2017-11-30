de87891bef849960381c55bad1c030d6533d2971
suozq1 <915856893@qq.com>
2017-11-30 11:18:03 +0800

    代码微调

--- spark/src/main/java/com/suozq/spark/SparkContextHolder.java
+++ spark/src/main/java/com/suozq/spark/SparkContextHolder.java
@@ -11,7 +11,14 @@ public class SparkContextHolder {
 	
 	public static JavaSparkContext getSparkContext(){
 		if(sc==null){
-			SparkConf conf=new SparkConf().setMaster("local").setAppName("SparkTest");
+			SparkConf conf=new SparkConf().setMaster("spark://10.99.35.23:7077").setAppName("SparkApp");
+			
+			//引入效率更高的序列化代码类，比java序列化速度更快压缩率更高
+			//conf.set("spark.serializer","org. apache. spark. serializer. KryoSerializer");
+			//conf.set("spark. kryo. registrationRequired","true");
+			//conf.registerKryoClasses(new Class<?>[]{String.class});
+
+
 			sc=new JavaSparkContext(conf);
 		}
 		return sc;
--- spark/src/main/java/com/suozq/spark/io/TxtFileReader.java
+++ spark/src/main/java/com/suozq/spark/io/TxtFileReader.java
@@ -1,15 +1,20 @@
 package com.suozq.spark.io;
 
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-
-import com.suozq.spark.SparkContextHolder;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
 
 public class TxtFileReader {
 	public static void main(String[] args) {
-		JavaSparkContext sc = SparkContextHolder.getSparkContext();
+		SparkConf conf=new SparkConf().setAppName("SparkApp").setMaster("spark://10.99.35.23:7077");
+		SparkContext sc=new SparkContext(conf);
+		//SparkSession spark = SparkSession.builder().appName("Simple Application").master("spark://10.99.35.23:7077").getOrCreate();
+		
+		//System.out.println(spark);
+	/*	//JavaSparkContext sc = SparkContextHolder.getSparkContext();
 		JavaRDD<String> xmlRdd = sc.textFile(TxtFileReader.class.getResource("/file/db.txt").toString());
 		JavaRDD<String> filterRdd = xmlRdd.filter(x -> x.contains("groupId")).map(x-> x.replaceAll("</?groupId>", ""));
-		filterRdd.saveAsTextFile(SparkContextHolder.SPARK_TEMP_DIR+"/groupId");
+		filterRdd.saveAsTextFile(SparkContextHolder.SPARK_TEMP_DIR+"/groupId");*/
+		sc.stop();
+		
 	}
 }
