bd0b4f2562a5029c209dee2bcb8a62919ebfdb5f
suozq1 <915856893@qq.com>
2018-01-23 15:57:57 +0800

    提交

--- onsite/test-server/src/main/java/com/lenovo/onsite/test/controller/TestController.java
+++ onsite/test-server/src/main/java/com/lenovo/onsite/test/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.lenovo.onsite.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,15 +11,23 @@ import com.lenovo.onsite.common.Response;
 import com.lenovo.onsite.test.service.UserService;
 
 @RestController
+@Configuration
 @RequestMapping("/test")
+@RefreshScope
 public class TestController {
 	
 	@Autowired
 	private UserService userService;
 	
+	@Value("${config.test.name}")
+	private String name;
+	
 	@RequestMapping("/hello")
 	public Response hello(String name){
 		Response r = new Response(0);
+		if(name==null||name.equals("")){
+			name=this.name;
+		}
 		r.setData(userService.get(name));
 		return r;
 	}
--- onsite/user-server/src/main/java/com/lenovo/onsite/user/controller/UserController.java
+++ onsite/user-server/src/main/java/com/lenovo/onsite/user/controller/UserController.java
@@ -1,18 +1,24 @@
 package com.lenovo.onsite.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lenovo.onsite.common.dto.UserDTO;
-
+@RefreshScope
 @RestController
 public class UserController {
     
+	@Value("${hello.world.name}")
+	private String name;
+	
 	@RequestMapping("/user/{name}")
 	public UserDTO getUser(@PathVariable("name") String name){
 		UserDTO u = new UserDTO();
-		u.setName("王媛媛");
+		System.err.println(this.name);
+		u.setName(name);
 		u.setAge(18);
 		u.setGender(0);
 		return u;
