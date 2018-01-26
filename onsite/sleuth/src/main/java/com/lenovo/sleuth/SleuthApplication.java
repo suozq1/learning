b3a891d56f869ec0477d185efd0ef9b70d57d2a1
suozq1 <915856893@qq.com>
2018-01-26 18:06:35 +0800

    sleuth

---
+++ onsite/sleuth/src/main/java/com/lenovo/sleuth/SleuthApplication.java
@@ -0,0 +1,20 @@
+package com.lenovo.sleuth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class SleuthApplication {
+	static Logger log=LoggerFactory.getLogger(SleuthApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthApplication.class, args);
+	}
+}
